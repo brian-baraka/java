cb61d8774c0abe5bf0f582517b0d34200f2c6bc7
brian nzuve <>
2022-10-27 15:03:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print ("Brian")
+print ("October 27th 2022")
+print ("""I am signing up for Replit's 100 days of python challenge
+I Will make sure to spend some time every day coding along, for a minimun of 10 minutes a day
+I'll be using Replit, an amazing Online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field! """)
+print ("You can follow my progress at replit.com/@David")
+print ("stdin")
